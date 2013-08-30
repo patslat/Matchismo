aebfdbcfc4b421fa6bf237ab3f2b1f76b80fe831
Patrick Slattery <slattery.patrickj@gmail.com>
2013-08-29 19:07:07 -0700

    Initial Commit

---
+++ Matchismo/CardGameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CardGameAppDelegate.h
+//  Matchismo
+//
+//  Created by Patrick Slattery on 8/29/13.
+//  Copyright (c) 2013 Patrick Slattery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by Patrick Slattery on 8/29/13.
+//  Copyright (c) 2013 Patrick Slattery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
